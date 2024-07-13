5d22adfdac49998e8346049eaff523520d57f152
AbdukarimK <abdukarimkhusenov@gmail.com>
2024-07-13 23:34:01 +0500

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.module.css'
+
+export default function Header(){
+    return (
+        <header>
+            <div className='intro'><p>Help people in Tampa</p></div>
+        </header>
+    )
+}
